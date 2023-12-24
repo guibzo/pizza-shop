6b2787e97e6670c7430b33738e4fea76098fe3b9
Guilherme Viana <imluizguilara@hotmail.com>
2023-12-24 14:08:40 -0300

    feat: add recharts grafic for period revenue

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -3,11 +3,12 @@ import { MonthRevenueCard } from "./(cards)/month-revenue";
 import { MonthOrdersAmountCard } from "./(cards)/month-orders-amount";
 import { DaysOrdersAmountCard } from "./(cards)/day-orders-amount";
 import { MonthCanceledOrdersAmountCard } from "./(cards)/month-canceled-orders-amount";
+import { RevenueChart } from "./revenue-chart";
 
 export const Dashboard = () => {
   return (
     <>
-      <Helmet title="Login" />
+      <Helmet title="Início" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 
@@ -16,6 +17,10 @@ export const Dashboard = () => {
           <MonthOrdersAmountCard />
           <DaysOrdersAmountCard />
           <MonthCanceledOrdersAmountCard />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
         </div>
       </div>
     </>
