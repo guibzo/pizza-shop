d3e5429a15a696d0fda88d8f0378d099e68fa3f1
Guilherme Viana <imluizguilara@hotmail.com>
2024-01-17 12:00:42 -0300

    feat: add dynamic data to header

---
+++ src/api/get-managed-restaurant.ts
@@ -0,0 +1,16 @@
+import { api } from '@/libs/axios'
+
+type GetManagedRestaurantResponse = {
+  id: string
+  name: string
+  createdAt: Date | null
+  updatedAt: Date | null
+  description: string | null
+  managerId: string | null
+}
+
+export async function getManagedRestaurant(): Promise<GetManagedRestaurantResponse> {
+  const response = await api.get('/managed-restaurant')
+
+  return response.data
+}
--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -4,4 +4,5 @@ import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true, // faz com que os cookies do front sejam enviados automaticamente pro back
 })
