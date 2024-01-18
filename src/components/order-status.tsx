029c5644fc7552ab2ba0dbd06b445ac804860efd
Guilherme Viana <imluizguilara@hotmail.com>
2024-01-18 12:36:01 -0300

    feat: add dynamic data to orders list

---
+++ src/components/order-status.tsx
@@ -0,0 +1,45 @@
+type OrderStatusList =
+  | 'pending'
+  | 'canceled'
+  | 'processing'
+  | 'delivering'
+  | 'delivered'
+
+type OrderStatusProps = {
+  status: OrderStatusList
+}
+
+const orderStatusMap: Record<OrderStatusList, string> = {
+  pending: 'Pendente',
+  canceled: 'Cancelado',
+  delivered: 'Entregue',
+  delivering: 'Em entrega',
+  processing: 'Em preparo',
+}
+
+export const OrderStatus = ({ status }: OrderStatusProps) => {
+  return (
+    <div className="flex items-center gap-2">
+      {status === 'pending' && (
+        <span className="h-2 w-2 rounded-full bg-slate-400" />
+      )}
+
+      {status === 'canceled' && (
+        <span className="h-2 w-2 rounded-full bg-rose-500" />
+      )}
+
+      {status === 'delivered' && (
+        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+      )}
+
+      {status === 'delivering' ||
+        (status === 'processing' && (
+          <span className="h-2 w-2 rounded-full bg-amber-500" />
+        ))}
+
+      <span className="font-medium text-muted-foreground">
+        {orderStatusMap[status]}
+      </span>
+    </div>
+  )
+}
