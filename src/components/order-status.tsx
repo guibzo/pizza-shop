85f66d9f745fa584a41c07ae6c9fb51172d8e1d3
Guilherme Viana <imluizguilara@hotmail.com>
2024-02-03 20:16:58 -0300

    test: add unit test for order-status

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -1,48 +1,63 @@
 export type OrderStatusList =
-  | 'pending'
-  | 'canceled'
-  | 'processing'
-  | 'delivering'
-  | 'delivered'
+  | "pending"
+  | "canceled"
+  | "processing"
+  | "delivering"
+  | "delivered";
 
 type OrderStatusProps = {
-  status: OrderStatusList
-}
+  status: OrderStatusList;
+};
 
 const orderStatusMap: Record<OrderStatusList, string> = {
-  pending: 'Pendente',
-  canceled: 'Cancelado',
-  delivered: 'Entregue',
-  delivering: 'Em entrega',
-  processing: 'Em preparo',
-}
+  pending: "Pendente",
+  canceled: "Cancelado",
+  delivered: "Entregue",
+  delivering: "Em entrega",
+  processing: "Em preparo",
+};
 
 export const OrderStatus = ({ status }: OrderStatusProps) => {
   return (
     <div className="flex items-center gap-2">
-      {status === 'pending' && (
-        <span className="h-2 w-2 rounded-full bg-slate-400" />
+      {status === "pending" && (
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-slate-400"
+        />
       )}
 
-      {status === 'processing' && (
-        <span className="h-2 w-2 rounded-full bg-amber-500" />
+      {status === "processing" && (
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-amber-500"
+        />
       )}
 
-      {status === 'delivering' && (
-        <span className="h-2 w-2 rounded-full bg-amber-500" />
+      {status === "delivering" && (
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-amber-500"
+        />
       )}
 
-      {status === 'delivered' && (
-        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+      {status === "delivered" && (
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-emerald-500"
+        />
       )}
 
-      {status === 'canceled' && (
-        <span className="h-2 w-2 rounded-full bg-rose-500" />
+      {status === "canceled" && (
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-rose-500"
+        />
       )}
 
       <span className="font-medium text-muted-foreground">
         {orderStatusMap[status]}
       </span>
     </div>
-  )
-}
+  );
+};
