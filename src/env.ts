93ef61988d87d14e386b8f361a6b60c3212225e9
Guilherme Viana <imluizguilara@hotmail.com>
2024-02-04 17:26:03 -0300

    chore: setup msw for tests

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,10 @@
 // to validate env variable values
-import { z } from 'zod'
+import { z } from "zod";
 
 const envSchema = z.object({
+  MODE: z.enum(["production", "development", "test"]),
   VITE_API_URL: z.string().url(),
-  VITE_ENABLE_API_DELAY: z.string().transform((val) => val === 'true'),
-})
+  VITE_ENABLE_API_DELAY: z.string().transform((val) => val === "true"),
+});
 
-export const env = envSchema.parse(import.meta.env)
+export const env = envSchema.parse(import.meta.env);
